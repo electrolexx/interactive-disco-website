e6cfa67b88f139a53641aab22b034de61229501d
Alex Desharnais <alex@arsonal.com>
2019-04-03 18:16:01 -0700

    save project sections layout progress

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,10 +3,27 @@ import Item from './Item';
 import './Section.css';
 
 export default function Section(props) {
+  const projectData = props.projectData;
   return (
     <div className="section-container">
       <div className="section-title">{props.title.toUpperCase()}</div>
-      <Item imageThumb={'web-white-princess.jpg'} client={'starz'} project={'the white princess'} />
+      <div className="projects-container">
+        <div className="projects-row">
+          {/* {Object.keys(projectData).map(key => console.log(key))} */}
+
+          <Item
+            imageThumb={'web-white-princess.jpg'}
+            client={'starz'}
+            project={'the white princess'}
+          />
+          <Item
+            imageThumb={'web-davincis-demons.jpg'}
+            client={'starz'}
+            project={"da vinci's demons"}
+          />
+          <Item imageThumb={'web-arsonal.jpg'} client={'arsonal'} project={'agency site'} />
+        </div>
+      </div>
     </div>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,10 +4,27 @@ import Section from '../components/Section';
 
 class Home extends Component {
   render() {
+    const featuredSection = {
+      project1: {
+        filename: 'web-white-princess.jpg',
+        client: 'starz',
+        project: 'the white princess',
+      },
+      project2: {
+        filename: 'web-davincis-demons.jpg',
+        client: 'starz',
+        project: "da vinci's demons",
+      },
+      project3: {
+        filename: 'web-arsonal.jpg',
+        client: 'arsonal',
+        project: 'agency site',
+      },
+    };
     return (
       <div className="Home">
         <Header />
-        <Section title={'featured'} />
+        <Section title={'featured'} projectData={featuredSection} />
       </div>
     );
   }
